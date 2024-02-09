f68fe751f2de30cc91ab57e3a37539287f2aaea5
manelkdm <manel.keddam01@gmail.com>
2024-02-09 12:35:53 -0500

    ADD config file

--- test_api.py
+++ test_api.py
@@ -2,23 +2,33 @@
 # the api is at http://localhost:8080/api
 
 import requests
+import configparser
+import os
+
+config = configparser.ConfigParser()
+config_path = 'config.ini'
+
+config.read(config_path)
+username = config['credentials']['username']
+password = config['credentials']['password']
 
 def call_api():
     url = 'http://localhost:8080/api/' 
     try:
         # we could prompt the user for the username and password
-        username = 'manel'
-        password = 'ThisIsAPassword'
 
         body = {
             "usernameOrEmail": username,
             "password": password
         }
         
-        response = requests.get(url, json=body)
+        response = requests.post(url, json=body)
         if response.status_code == 200:
+            print(f"Request successfull: {response.text}")
             return f"Request successfull: {response.text}"
+            
         else:
+            print(f"Request unsuccessfull: {response.status_code} {response.text}")
             return f"Request unsuccessfull: {response.status_code} {response.text}"
     except requests.RequestException as e:
         return f"Error: {e}"
