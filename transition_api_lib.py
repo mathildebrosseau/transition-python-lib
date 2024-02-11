29d265d74edddd7d692856f8f521dd1ef4ac1f8a
manelkdm <manel.keddam01@gmail.com>
2024-02-11 11:43:33 -0500

    add config path & clean code

--- transition_api_lib.py
+++ transition_api_lib.py
@@ -1,13 +1,22 @@
 import requests
 import configparser
-import json
 import geojson
+import os
 
-username = "manel"
-password = "ThisIsAPassword"
-url = 'http://localhost:8080/api/' 
 
-# we could prompt the user for the username and password
+# change working directory to the correct path
+config_path = os.path.join(os.path.dirname(__file__))
+os.chdir(config_path)
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+
+username = config['credentials']['username']
+password = config['credentials']['password']
+
+url = config['URL']['dev']
+
 body = {
     "usernameOrEmail": username,
     "password": password
@@ -18,11 +27,9 @@ def call_api():
         
         response = requests.post(url, json=body)
         if response.status_code == 200:
-            print(f"Request successfull: {response.text}")
             return f"Request successfull: {response.text}"
             
         else:
-            print(f"Request unsuccessfull: {response.status_code} {response.text}")
             return f"Request unsuccessfull: {response.status_code} {response.text}"
     except requests.RequestException as e:
         return f"Error: {e}"
@@ -32,56 +39,47 @@ def get_transition_paths():
     try:
         geojson_file = requests.post(url+"paths", json=body)
         if geojson_file.status_code == 200:
-            print(f"Paths request successfull")
             result = geojson.loads(geojson_file.text)
             return result
         else:
-            print(f"Paths request unsuccessfull: {geojson_file.status_code} {geojson_file.text}")
             return f"Request unsuccessfull: {geojson_file.status_code} {geojson_file.text}"
     except requests.RequestException as e:
         return f"Error: {e}"
-    
+
+
 def get_transition_nodes():
     try:
         geojson_file = requests.post(url+"nodes", json=body)
         if geojson_file.status_code == 200:
-            print(f"Nodes request successfull")
             result = geojson.loads(geojson_file.text)
             return result
         else:
-            print(f"Nodes request unsuccessfull: {geojson_file.status_code} {geojson_file.text}")
-            return f"Request unsuccessfull: {geojson_file.status_code} {geojson_file.text}"
-    except requests.RequestException as e:
-        return f"Error: {e}"
-    
-def get_transition_scenarios():
-    try:
-        geojson_file = requests.post(url+"scenarios", json=body)
-        if geojson_file.status_code == 200:
-            print(f"Scenarios request successfull")
-            result = geojson.loads(geojson_file.text)
-            return result
-        else:
-            print(f"Scenarios request unsuccessfull: {geojson_file.status_code} {geojson_file.text}")
-            return f"Request unsuccessfull: {geojson_file.status_code} {geojson_file.text}"
-    except requests.RequestException as e:
-        return f"Error: {e}"
-    
-def get_transition_routing_modes():
-    try:
-        geojson_file = requests.post(url+"routing-modes", json=body)
-        if geojson_file.status_code == 200:
-            print(f"Routing modes request successfull")
-            result = geojson.loads(geojson_file.text)
-            return result
-        else:
-            print(f"Routing modes request unsuccessfull: {geojson_file.status_code} {geojson_file.text}")
             return f"Request unsuccessfull: {geojson_file.status_code} {geojson_file.text}"
     except requests.RequestException as e:
         return f"Error: {e}"
     
 
-get_transition_paths()
-get_transition_nodes()
-get_transition_scenarios()
-get_transition_routing_modes()
+def get_transition_scenarios():
+    try:
+        result = requests.post(url+"scenarios", json=body)
+        if result.status_code == 200:
+            return result
+        else:
+            return f"Request unsuccessfull: {result.status_code} {result.text}"
+    except requests.RequestException as e:
+        return f"Error: {e}"
+   
+    
+def get_transition_routing_modes():
+    try:
+        result = requests.post(url+"routing-modes", json=body)
+        if result.status_code == 200:
+            # Query returns the list as a string, so we need to parse it.
+            # Can probably be done differently
+            result = [x.replace("[", "").replace("]", "").replace('"', "") for x in result.text.split(",")]
+            return result
+        else:
+            print(f"Routing modes request unsuccessfull: {result.status_code} {result.text}")
+            return f"Request unsuccessfull: {result.status_code} {result.text}"
+    except requests.RequestException as e:
+        return f"Error: {e}"
