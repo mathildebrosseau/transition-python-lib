1c9663de61a4b02f108d78c5a66897f0a119e818
manelkdm <manel.keddam01@gmail.com>
2024-02-15 14:43:40 -0500

    Initialisation librairie

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='transition_lib',
+    version='0.1.0',    
+    description='',
+    url='',
+    author='',
+    author_email='',
+    packages=['transition_lib'],
+    install_requires=['mpi4py>=2.0',
+                      'numpy',                     
+                      ],
+
+    classifiers=[
+        'License :: OSI Approved :: MIT License',  
+        'Operating System :: Linux', 
+        'Programming Language :: Python :: 3',
+    ],
+)
