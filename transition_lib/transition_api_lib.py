96ffd3aff41b42efd823f6f30251ebfe91feabd2
Mathilde Brosseau <mathilde.brosseau@polymtl.ca>
2024-02-20 15:43:49 -0500

    Added token handling for authentification

--- transition_lib/transition_api_lib.py
+++ transition_lib/transition_api_lib.py
@@ -3,8 +3,8 @@ import geojson
 import os
 
 class Transition:
-
-    DEFAULT_URL = "http://localhost:8080/api/"
+    DEFAULT_URL = "http://localhost:8080"
+    API_URL = f"{DEFAULT_URL}/api"
     
     @staticmethod
     def build_body():
@@ -24,7 +24,9 @@ class Transition:
     def build_headers():
         token = os.environ.get('TRANSITION_TOKEN')
         if token is None:
-            raise ValueError("No token found.")
+            Transition.get_token()
+            token = os.environ.get('TRANSITION_TOKEN')
+        
         headers = {
             "Authorization": f"Bearer {token}"
         }
@@ -33,13 +35,13 @@ class Transition:
     @staticmethod
     def call_api():
         try:
-            body = Transition.build_body()
-            response = requests.post(Transition.DEFAULT_URL, json=body)
+            headers = Transition.build_headers()
+            response = requests.get(Transition.API_URL, headers=headers)
             if response.status_code == 200:
-                print(f"Request successfull: {response.text}")
+                print(f"Request to /api successfull: {response.text}")
                 return response
             else:
-                print(f"Request unsuccessfull: {response.status_code} {response.text}")
+                print(f"Request to /api unsuccessfull: {response.status_code} {response.text}")
                 return response
         except requests.RequestException as error:
             return error
@@ -47,91 +49,68 @@ class Transition:
     @staticmethod
     def get_transition_paths():
         try:
-            body = Transition.build_body()
-            geojson_file = requests.post(f"{Transition.DEFAULT_URL}paths", json=body)
+            headers = Transition.build_headers()
+            geojson_file = requests.get(f"{Transition.API_URL}/paths", headers=headers)
             if geojson_file.status_code == 200:
                 result = geojson.loads(geojson_file.text)
                 return result
             else:
-                return f"Request unsuccessfull: {geojson_file.status_code} {geojson_file.text}"
+                return f"Request to /paths unsuccessfull: {geojson_file.status_code} {geojson_file.text}"
         except requests.RequestException as error:
             return error
         
     @staticmethod
     def get_transition_nodes():
         try:
-            body = Transition.build_body()
-            geojson_file = requests.post(f"{Transition.DEFAULT_URL}nodes", json=body)
+            headers = Transition.build_headers()
+            geojson_file = requests.get(f"{Transition.API_URL}/nodes", headers=headers)
             if geojson_file.status_code == 200:
                 result = geojson.loads(geojson_file.text)
                 return result
             else:
-                return f"Request unsuccessfull: {geojson_file.status_code} {geojson_file.text}"
+                return f"Request to /nodes unsuccessfull: {geojson_file.status_code} {geojson_file.text}"
         except requests.RequestException as error:
             return error
     
     @staticmethod
     def get_transition_scenarios():
         try:
-            body = Transition.build_body()
-            result = requests.post(f"{Transition.DEFAULT_URL}scenarios", json=body)
+            headers = Transition.build_headers()
+            result = requests.get(f"{Transition.API_URL}/scenarios", headers=headers)
             if result.status_code == 200:
                 scenarios = [entry['name'] for entry in result.json()['collection']]
                 return scenarios
             else:
-                return f"Request unsuccessfull: {result.status_code} {result.text}"
+                return f"Request to /scenarios unsuccessfull: {result.status_code} {result.text}"
         except requests.RequestException as error:
             return error
         
     @staticmethod    
     def get_transition_routing_modes():
         try:
-            body = Transition.build_body()
-            result = requests.post(f"{Transition.DEFAULT_URL}routing-modes", json=body)
+            headers = Transition.build_headers()
+            result = requests.get(f"{Transition.API_URL}/routing-modes", headers=headers)
             if result.status_code == 200:
                 # Query returns the list as a string, so we need to parse it.
                 # Can probably be done differently
                 result = [x.replace("[", "").replace("]", "").replace('"', "") for x in result.text.split(",")]
                 return result
             else:
-                print(f"Routing modes request unsuccessfull: {result.status_code} {result.text}")
-                return f"Request unsuccessfull: {result.status_code} {result.text}"
+                return f"Request to /routing-modes unsuccessfull: {result.status_code} {result.text}"
         except requests.RequestException as error:
             return error
         
-    
-    # The following methods are not used with the current endpoint implementation,
-    # but will be used when tha API is updated to include the token.
-
-    # @staticmethod     
-    # def call_api_queries():
-    #     try:
-    #         headers = Transition.build_headers()
-    #         response = requests.post(f"{Transition.DEFAULT_URL}queries", headers=Transition.headers)
-    #         if response.status_code == 200:
-    #             print(f"Request successfull: {response.text}")
-    #             return f"Request successfull: {response.text}"
-    #         else:
-    #             print(f"Request unsuccessfull: {response.status_code} {response.text}")
-    #             return f"Request unsuccessfull: {response.status_code} {response.text}"
-    #     except requests.RequestException as e:
-    #         return f"Error: {e}"
-        
-    # @staticmethod
-    # def get_token():
-    #     try:
-    #         body = Transition.build_body()
-    #         response = requests.get(f"{Transition.DEFAULT_URL}token", json=body)
-    #         if response.status_code == 200:
-    #             # Update the token in the config file
-    #             config['credentials']['token'] = response.text
-    #             with open('config.ini', 'w') as configfile:
-    #                 config.write(configfile)
-    #             return f"Request successfull: {response.text}"
-    #         else:
-    #             print(f"Request unsuccessfull: {response.status_code} {response.text}")
-    #             return f"Request unsuccessfull: {response.status_code} {response.text}"
-    #     except requests.RequestException as e:
-    #         return f"Error: {e}"
+    @staticmethod
+    def get_token():
+        try:
+            body = Transition.build_body()
+            response = requests.post(f"{Transition.DEFAULT_URL}/token", json=body)
+            if response.status_code == 200:
+                os.environ['TRANSITION_TOKEN'] = response.text
+                return f"Request to /token successfull: {response.text}"
+            else:
+                return f"Request to /token unsuccessfull: {response.status_code} {response.text}"
+        except requests.RequestException as error:
+            return error
         
 
