0de02fe3dc54adafad228d801b5b2a703cce875a
manelkdm <manel.keddam01@gmail.com>
2024-02-22 14:46:39 -0500

    Add token handling
    Add token handling
    
    Add URL

--- transition_lib/transition_api_lib.py
+++ transition_lib/transition_api_lib.py
@@ -1,10 +1,40 @@
 import requests
 import geojson
 import os
+import configparser
+
+
 
 class Transition:
     BASE_URL = "http://localhost:8080"
     API_URL = f"{BASE_URL}/api"
+
+    config = configparser.ConfigParser()
+    config_path = os.path.join(os.path.dirname(__file__), 'config.ini')
+    
+    if not os.path.isfile(config_path):
+        config['credentials'] = {
+            'username': '',
+            'token': ''
+        }
+        config['URL'] = {
+            'base_url': 'http://localhost:8080',
+        }
+        with open(config_path, 'w') as configfile:
+            config.write(configfile)
+    else:
+        config.read(config_path)
+
+    @staticmethod
+    def set_base_url():
+        url = os.environ.get('TRANSITION_BASE_URL')
+        if url is not None and url != "":
+            Transition.BASE_URL = url
+            Transition.API_URL = f"{Transition.BASE_URL}/api"
+            Transition.config['URL']['base_url'] = Transition.BASE_URL
+            with open(Transition.config_path, 'w') as configfile:
+                Transition.config.write(configfile)
+
     
     @staticmethod
     def build_body():
@@ -13,23 +43,35 @@ class Transition:
 
         if username is None or password is None:
             raise ValueError("Username or password not set.")
+
+        # save the username in the config file
+        Transition.config['credentials']['username'] = username
+
+        with open(Transition.config_path, 'w') as configfile:
+            Transition.config.write(configfile)
         
         body = {
             "usernameOrEmail": username,
             "password": password
         }
-        return body
+
+        return body            
 
     @staticmethod
     def build_headers():
-        token = os.environ.get('TRANSITION_TOKEN')
-        if token is None:
-            Transition.get_token()
-            token = os.environ.get('TRANSITION_TOKEN')
+        token = Transition.config['credentials']['token']
+
+        if token is None or token == "":
+            token = Transition.get_token().text
+            Transition.config['credentials']['token'] = token
+
+            with open(Transition.config_path, 'w') as configfile:
+                Transition.config.write(configfile)
         
         headers = {
             "Authorization": f"Bearer {token}"
         }
+
         return headers
 
     @staticmethod
@@ -103,14 +145,15 @@ class Transition:
     @staticmethod
     def get_token():
         try:
+            Transition.set_base_url()
             body = Transition.build_body()
             response = requests.post(f"{Transition.BASE_URL}/token", json=body)
+            print(response.text)
             if response.status_code == 200:
                 os.environ['TRANSITION_TOKEN'] = response.text
-                return f"Request to /token successfull: {response.text}"
+                return response
             else:
-                return f"Request to /token unsuccessfull: {response.status_code} {response.text}"
+                return response
         except requests.RequestException as error:
             return error
         
-
