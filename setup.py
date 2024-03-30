8c283105d6af2f7ed9b58472e756a191132132c9
Mathilde Brosseau <mathilde.brosseau@polymtl.ca>
2024-03-29 22:29:22 -0400

    version 0.1.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='transition-lib-test',
-    version='0.1.3',    
+    version='0.1.4',    
     description='A Python package to interact with the Transition API.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
