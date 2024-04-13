1788d35769dc3afa5c81e401000e7efcc041a63b
Mathilde Brosseau <mathilde.brosseau@polymtl.ca>
2024-04-13 00:24:02 -0400

    Add docstring

--- pyTransition/transition.py
+++ pyTransition/transition.py
@@ -38,8 +38,19 @@ class Transition:
         else:
             self.token = self.__request_token(username, password)
 
-    # TODO : ajouter self
-    def __build_body(self, username, password):
+    def __build_authentication_body(self, username, password):
+        """Builds the body for the token request.
+
+        Args:
+            username (string): user's username
+            password (string): user's password
+
+        Raises:
+            ValueError: When username or password is empty
+
+        Returns:
+            dict: A dict representing the body of the token request
+        """
         if username is None or password is None:
             raise ValueError("Username or password empty.")
 
@@ -51,6 +62,14 @@ class Transition:
         return body            
 
     def __build_headers(self):
+        """Build the headers with required authorizations for all Transition api requests.
+
+        Raises:
+            ValueError: When the user's token is not defined or empty
+
+        Returns:
+            dict: A dict containing the headers for all api requests.
+        """
         if self.token is None or self.token == "":
             raise ValueError("Token not set.")
         
@@ -61,30 +80,59 @@ class Transition:
         return headers
 
     def __request_token(self, username, password):
-        body = self.__build_body(username, password)
+        """Authenticates the user with the Transition api to obtain an API authentication token.
+
+        Args:
+            username (string): user's username or email
+            password (string): user's password
+
+        Returns:
+            string: the user's authentication token
+        """
+        body = self.__build_authentication_body(username, password)
         response = requests.post(f"{self.base_url}/token", json=body)
         response.raise_for_status()
         return response.text
 
-    def get_paths(self):      
+    def get_paths(self):
+        """Gets all paths currently loaded in the Transition application
+
+        Returns:
+            geojson: Transition paths as a GeoJSON LineString FeatureCollection object
+        """
         headers = self.__build_headers()
         response = requests.get(f"{self.base_url}/api/paths", headers=headers)
         response.raise_for_status()
         return response.json()
 
     def get_nodes(self):
+        """Gets all nodes currently loaded in the Transition application
+
+        Returns:
+            geojson: Transition nodes as a GeoJSON Point FeatureCollection object
+        """
         headers = self.__build_headers()
         response = requests.get(f"{self.base_url}/api/nodes", headers=headers)
         response.raise_for_status()
         return response.json()
 
     def get_scenarios(self):
+        """Gets all scenarios currently loaded in the Transition application
+
+        Returns:
+            List: List of Transition scenarios with their parameters
+        """
         headers = self.__build_headers()
         response = requests.get(f"{self.base_url}/api/scenarios", headers=headers)
         response.raise_for_status()
         return response.json()
  
     def get_routing_modes(self):
+        """Gets all routing modes currently available in the Transition application
+
+        Returns:
+            List: List of routing modes as a list of strings
+        """
         headers = self.__build_headers()
         response = requests.get(f"{self.base_url}/api/routing-modes", headers=headers)
         response.raise_for_status()
@@ -104,6 +152,26 @@ class Transition:
                                max_first_waiting_time_minutes, 
                                with_geojson,
                                with_alternatives):
+        """Calculates a route between a given origin and destination with the provided modes of transport
+
+        Args:
+            modes (List[string]): List of all modes of transport for which to do the routing calculation.
+            origin (List[float]): Coordinates of the route origin as [longitude, latitude]
+            destination (List[float]): Coordinates of the route destination as [longitude, latitude]
+            scenario_id (string): ID of the used scenario as loaded in Transition application.
+            departure_or_arrival_choice (string): Specifies whether the used time is "Departure" or "Arrival". Possible values are "Departure" or "Arrival".
+            departure_or_arrival_time (time): Departure or arrival time of the trip
+            max_travel_time_minutes (int): Maximum travel time including access, in minutes
+            min_waiting_time_minutes (int): Minimum waiting time, in minutes
+            max_transfer_time_minutes (int): Maximum transfer time, in minutes
+            max_access_time_minutes (int): Maximum access time, in minutes
+            max_first_waiting_time_minutes (int): Maximum wait time at first transit stop, in minutes
+            with_geojson (bool): If True, the returned JSON file will contain the "pathsGeojson" key for each mode
+            with_alternatives (bool): Indicates if the calculation must include alternative routes or not
+
+        Returns:
+            JSON: Route for each transit mode in JSON format
+        """
         departure_or_arrival_time = departure_or_arrival_time.hour * 3600 + departure_or_arrival_time.minute * 60 + departure_or_arrival_time.second
         departure_time = departure_or_arrival_time if departure_or_arrival_choice == "Departure" else None
         arrival_time = departure_or_arrival_time if departure_or_arrival_choice == "Arrival" else None
@@ -145,7 +213,6 @@ class Transition:
                                   n_polygons,
                                   delta_minutes,
                                   delta_interval_minutes,
-                                  place_name,
                                   max_total_travel_time_minutes,
                                   min_waiting_time_minutes,
                                   max_access_egress_travel_time_minutes,
@@ -153,6 +220,27 @@ class Transition:
                                   max_first_waiting_time_minutes,
                                   walking_speed_kmh,
                                   with_geojson):
+        """Calculates an accessibility map from a given origin
+
+        Args:
+            coordinates (List[float]): Coordinates of the map origin as [longitude, latitude]
+            scenario_id (string): ID of the used scenario as loaded in Transition application.
+            departure_or_arrival_choice (string): Specifies whether the used time is "Departure" or "Arrival". Possible values are "Departure" or "Arrival".
+            departure_or_arrival_time (time): Departure or arrival time of the trip
+            n_polygons (int): Number of polygons to be calculated
+            delta_minutes (int): Baseline delta used for average accessibility map calculations
+            delta_interval_minutes (int): Interval used between each calculation
+            max_total_travel_time_minutes (int): Maximum travel time, in minutes
+            min_waiting_time_minutes (int): Minimum waiting time, in minutes
+            max_access_egress_travel_time_minutes (int): Maximum access time, in minutes
+            max_transfer_travel_time_minutes (int): Maximum transfer time, in minutes
+            max_first_waiting_time_minutes (int): Maximum wait time at first transit stop, in minutes
+            walking_speed_kmh (float): Walking speed, in km/h
+            with_geojson (true): If True, the returned JSON file will contain the "pathsGeojson" key for each mode
+
+        Returns:
+            JSON: Accessibility map information in JSON format
+        """
         departure_or_arrival_time_seconds_from_midnight = departure_or_arrival_time.hour * 3600 + departure_or_arrival_time.minute * 60 + departure_or_arrival_time.second
         departure_time_seconds = departure_or_arrival_time_seconds_from_midnight if departure_or_arrival_choice == "Departure" else None
         arrival_time_seconds = departure_or_arrival_time_seconds_from_midnight if departure_or_arrival_choice == "Arrival" else None
